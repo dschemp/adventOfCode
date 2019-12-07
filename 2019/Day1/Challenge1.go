4fa7098850b668c78eab52cc00e476aa1d15b520
Daniel <7892612+dschemp@users.noreply.github.com>
2019-12-07 08:00:24 +0100

    Create Challenge1.go

---
+++ 2019/Day1/Challenge1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	f.Close()
+
+
+	var count int64
+
+	for _, eachline := range txtlines {
+		i, _ := strconv.ParseInt(eachline, 10, 32)
+		count += calcFuelCost(i)
+	}
+
+	fmt.Println(count)
+}
+
+func calcFuelCost(mass int64) int64 {
+	m := (mass / 3) - 2
+	if m <= 0 {
+		return 0
+	}
+	return m
+}
