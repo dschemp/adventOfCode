c635e553a452276b1d22da750ba73499fc041622
Daniel Schemp <7892612+dschemp@users.noreply.github.com>
2020-12-01 07:05:00 +0100

    added: Day 1

---
+++ 2020/Day 1/Challenge1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLines = File.ReadAllLines(@"input.txt");
+            int[] numbers = inputLines.Select(int.Parse).ToArray();
+            int maxSum = 2020;
+            bool found = false;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    int x = numbers[i];
+                    int y = numbers[j];
+
+                    if (x + y == maxSum)
+                    {
+                        Console.WriteLine($"Found x: {x} | y: {y} | x*y={x*y}");
+                        found = true;
+                    }
+
+                    if (found)
+                    {
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Nothing found!");
+            }
+        }
+    }
+}
---
+++ 2020/Day 1/Challenge2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLines = File.ReadAllLines(@"input.txt");
+            int[] numbers = inputLines.Select(int.Parse).ToArray();
+            int maxSum = 2020;
+            bool found = false;
+
+            for (int i = 0; i < numbers.Length - 2; i++)
+            {
+                int x = numbers[i];
+                
+                for (int j = i; j < numbers.Length - 1; j++)
+                {
+                    int y = numbers[j];
+
+                    if (x + y > maxSum)
+                    {
+                        continue;
+                    }
+
+                    for (int k = j; k < numbers.Length; k++)
+                    {
+                        int z = numbers[k];
+
+                        if (x + y + z == maxSum)
+                        {
+                            Console.WriteLine($"Found x: {x} | y: {y} | z:{z} | x*y*z={x*y*z}");
+                            found = true;
+                        }
+
+                        if (found)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (found)
+                    {
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Nothing found!");
+            }
+        }
+    }
+}
