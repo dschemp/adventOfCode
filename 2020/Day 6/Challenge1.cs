6d684ddad5bf76754a3cc5f6025480f3c08da5e7
Daniel Schemp <secure@dschemp.eu>
2020-12-06 06:17:37 +0100

    added: Day 6

---
+++ 2020/Day 6/Challenge1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+
+            IEnumerable<string> cleanedInput = CleanUpInput(lines);
+            IEnumerable<string> cleanedGroupInput = CombineGroupAnswers(cleanedInput);
+
+            int sum = cleanedGroupInput.Sum(s => s.Length);
+            Console.WriteLine($"Sum of questions answered with yes: {sum}");
+        }
+
+        static IEnumerable<string> CleanUpInput(string[] lines)
+        {
+            string buffer = string.Empty;
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line) && !string.IsNullOrWhiteSpace(buffer))
+                {
+                    yield return buffer;
+                    buffer = string.Empty;
+                }
+                else
+                {
+                    buffer = string.Join("", buffer.Trim(), line?.Trim());
+                }
+            }
+
+            yield return buffer;
+        }
+
+        static IEnumerable<string> CombineGroupAnswers(IEnumerable<string> groups)
+        {
+            foreach (var group in groups)
+            {
+                string buffer = string.Empty;
+                foreach (var c in group)
+                {
+                    if (!buffer.Contains(c))
+                    {
+                        buffer += c;
+                    }
+                }
+
+                yield return buffer;
+            }
+        }
+    }
+}
---
+++ 2020/Day 6/Challenge2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+
+            IEnumerable<string> cleanedInput = CleanUpInput(lines).ToList();
+            IEnumerable<string> cleanedGroupInput = CombineGroupAnswers(cleanedInput);
+
+            int sum = cleanedGroupInput.Sum(s => s.Length);
+            Console.WriteLine($"Sum of all questions answered with yes: {sum}");
+        }
+
+        static IEnumerable<string> CleanUpInput(string[] lines)
+        {
+            string buffer = string.Empty;
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line) && !string.IsNullOrWhiteSpace(buffer))
+                {
+                    yield return buffer;
+                    buffer = string.Empty;
+                }
+                else
+                {
+                    buffer = string.Join("|", line?.Trim(), buffer.Trim()).Trim('|');
+                }
+            }
+
+            yield return buffer;
+        }
+
+        static IEnumerable<string> CombineGroupAnswers(IEnumerable<string> groups)
+        {
+            foreach (var group in groups)
+            {
+                string buffer = string.Empty;
+                string[] individualAnswers = group.Split('|').ToArray();
+
+                foreach (var c in individualAnswers[0])
+                {
+                    if (individualAnswers.All(t => t.Contains(c)))
+                    {
+                        if (!buffer.Contains(c))
+                        {
+                            buffer += c;
+                        }
+                    }
+                }
+
+                yield return buffer;
+            }
+        }
+    }
+}
