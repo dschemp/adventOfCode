650956c4addd75c562fc7cafe617819fdc84334e
Daniel Schemp <secure@dschemp.eu>
2020-12-13 17:06:34 +0100

    added: Day 10

---
+++ 2020/Day 10/Challenge1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            List<int> outputJolts = lines.Select(int.Parse).ToList();
+
+            int maxChargerJolts = outputJolts.Max();
+            int ownChargerJolts = maxChargerJolts + 3;
+
+            Console.WriteLine($"Max Charger J: {maxChargerJolts} | Own Charger J: {ownChargerJolts}");
+
+            outputJolts.Add(0);
+            outputJolts.Add(ownChargerJolts);
+
+            List<int> orderedChargers = outputJolts.OrderBy(t => t).ToList();
+
+            Dictionary<int, int> differences = new Dictionary<int, int> { { 1, 0 }, { 2, 0 }, { 3, 0 } };
+
+            for (int i = 0; i < orderedChargers.Count - 1; i++)
+            {
+                int diff = orderedChargers[i + 1] - orderedChargers[i];
+                differences[diff]++;
+            }
+            var diffTexts = differences.Select(kvp => kvp.Key + ":" + kvp.Value.ToString());
+
+            Console.WriteLine($"Differences: {string.Join(" | ", diffTexts)}");
+
+            Console.WriteLine($"N(1) x N(3) = {differences[1] * differences[3]}");
+        }
+    }
+}
---
+++ 2020/Day 10/Challenge2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            List<int> outputJolts = lines.Select(int.Parse).ToList();
+
+            int maxChargerJolts = outputJolts.Max();
+            int ownChargerJolts = maxChargerJolts + 3;
+
+            Console.WriteLine($"Max Charger J: {maxChargerJolts} | Own Charger J: {ownChargerJolts}");
+
+            outputJolts.Add(0);
+            outputJolts.Add(ownChargerJolts);
+
+            List<int> orderedChargers = outputJolts.OrderBy(t => t).ToList();
+
+            long amount = GetAmountOfValidCombinations(orderedChargers);
+
+            Console.WriteLine($"Amount of valid combinations: {amount}");
+        }
+
+        /*
+         * Thanks for this solution go out to lukasj3731
+         *
+         * His Solution:
+         * https://github.com/lukasj3731/Advent_of_Code/blob/master/AOC2020/src/AOC10.java
+         *
+         */
+        static long GetAmountOfValidCombinations(List<int> chargers)
+        {
+            Dictionary<int, long> mapping = new Dictionary<int, long>();
+            int ownChargerJolts = chargers.Max();
+
+            mapping.Add(ownChargerJolts, 1);
+
+            for (int i = chargers.Count - 2; i >= 0; i--)
+            {
+                int currentCharger = chargers[i];
+
+                long total = 0;
+                for (int j = 1; j <= 3; j++)
+                {
+                    if (mapping.ContainsKey(currentCharger + j))
+                    {
+                        total += mapping[currentCharger + j];
+                    }
+                }
+                mapping.Add(currentCharger, total);
+            }
+
+            return mapping[0];
+        }
+    }
+}
